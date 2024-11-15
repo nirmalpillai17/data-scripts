820fe55b8dc774d6bbde1556eac394b07d1bba1e
Nirmal S <nirmalpillai24@gmail.com>
2024-11-15 20:26:50 +0530

    initial commit

---
+++ ap_data/src/main.py
@@ -0,0 +1,38 @@
+import csv
+
+import os
+import pathlib
+
+
+base_path = pathlib.Path().absolute().parent
+
+data_path = base_path / "data_2"
+
+files = os.scandir(data_path)
+
+data_set = dict()
+
+for entry in files:
+    file = open(entry.path)
+    csv_reader = csv.reader(file)
+    
+    row = csv_reader.__next__()
+    model_index = row.index("MODEL")
+    firmware_index = row.index("FIRMWARE VERSION")
+
+    for row in csv_reader:
+        if data_set.get(row[model_index]):
+            data_set[row[model_index]].add(row[firmware_index])
+        else:
+            data_set[row[model_index]] = {row[firmware_index]}
+
+    file.close()
+
+out = open(base_path / "output_2.csv", "w")
+
+csv_writer = csv.writer(out)
+
+for item in data_set:
+    csv_writer.writerow([item, *data_set[item]])
+
+out.close() 
